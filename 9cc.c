a7ad28b2387d59216318953bd84affc6e502928c
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2021-08-22 12:07:09 +0900

    refactor:format to google style

--- 9cc.c
+++ 9cc.c
@@ -1,41 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
-{
-	if (argc != 2)
-	{
-		fprintf(stderr, "引数の数が不正です");
-		return 1;
-	}
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "引数の数が不正です");
+    return 1;
+  }
 
-	char *p = argv[1];
+  char *p = argv[1];
 
-	printf(".intel_syntax noprefix\n");
-	printf(".globl main\n");
-	printf("main:\n");
-	printf("  mov rax, %ld\n", strtol(p, &p, 10));
+  printf(".intel_syntax noprefix\n");
+  printf(".globl main\n");
+  printf("main:\n");
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-	while (*p)
-	{
-		if (*p == '+')
-		{
-			p++;
-			printf("  add rax, %ld\n", strtol(p, &p, 10));
-			continue;
-		}
-		if (*p == '-')
-		{
-			p++;
-			printf("  sub rax, %ld\n", strtol(p, &p, 10));
-			continue;
-		}
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
 
-		fprintf(stderr, "予期しない文字です: %c\n", *p);
-		return 1;
-	}
+    fprintf(stderr, "予期しない文字です: %c\n", *p);
+    return 1;
+  }
 
-	printf("  ret\n");
+  printf("  ret\n");
 
-	return 0;
+  return 0;
 }
